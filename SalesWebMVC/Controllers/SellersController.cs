15ce7dc14c85179ad983cf76fd8214295a4b206b
dimas9753 <dimasd9753@gmail.com>
2019-09-22 14:03:56 -0300

    Criação caixa Department no Cadastro Seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,21 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Services;
+using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerservice;
+        private readonly DepartmentService _departmentservice;
 
-        public SellersController(SellerService sellerservice)
+        public SellersController(SellerService sellerservice, DepartmentService departmentservice)
         {
             _sellerservice = sellerservice;
+            _departmentservice = departmentservice;
         }
         public IActionResult Index()
         {
             var list = _sellerservice.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() // GET
+        {
+            var departments = _departmentservice.FindAll();
+            var viewmodel = new SellerFormViewModel { Departments = departments };
+            return View(viewmodel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) // POST
+        {
+            _sellerservice.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
